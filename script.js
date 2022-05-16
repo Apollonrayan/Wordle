e6814df27550560659c4328c5c7b84386e7a1d4d
unknown <Administrateur@OPV01649.academie-paris.in.ac-paris.fr>
2022-05-16 16:48:35 +0200

    fix

--- script.js
+++ script.js
@@ -10006,7 +10006,7 @@ const offsetFromDate = new Date(2022, 0, 1)
 const msOffset = Date.now() - offsetFromDate
 const dayOffset = msOffset / 1000 / 60 / 60 / 24
 const targetWord = targetWords[Math.floor(dayOffset)]
-console.log(targetWord)
+console.log(targetWord);
 
 startInteraction()
 function startInteraction() {
